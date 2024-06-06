3944f577f7fa97dcbfa14623dd9b52f404379c70
Daniel <espartan.95.os@gmail.com>
2024-06-05 21:56:35 -0500

    segundoAvance

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+const cargarCabecero = () => {
+    var presupuesto = totalIngreso () - totalEgresos ()
+    var porcentajeEgreso = totalIngreso () / totalEgresos ()
+}
+
+
+const totalIngresos = () => {
+   // var ingresos = 900.00
+
+
+}
+
+var ingresos = {
+    Quincena: 9000,
+    Ropa: 400
+};
+
+for ( var totalIngreso, i=0; i<5; i++ );{
+console.log(totalIngreso)
+}
+
+
+
+    
+
