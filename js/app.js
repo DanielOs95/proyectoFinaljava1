c40ec425c236eb51e36f8f9dbd24e32cbd896af5
Daniel <espartan.95.os@gmail.com>
2024-06-24 22:13:22 -0500

    avanceFinal6

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { Ingreso } from './Ingreso.js';
+ import { Ingreso } from './Ingreso.js';
 import { Egreso } from './Egreso.js';
 
 let ingresos = [
@@ -12,6 +12,8 @@ let egresos = [
 ];
 
 const cargarApp = () => {
+    Ingreso.contadorIngresos = ingresos.length;
+    Egreso.contadorEgresos = egresos.length;
     cargarCabecero();
     cargarIngresos();
     cargarEgresos();
@@ -66,7 +68,7 @@ const crearIngresoHTML = (ingreso) => {
             <div class="derecha limpiarEstilos">
                 <div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>
                 <div class="elemento_eliminar">
-                    <button class="elemento_eliminar_btn" onclick="eliminarIngreso(${ingreso.id})">
+                    <button class="elemento_eliminar--btn" onclick="eliminarIngreso(${ingreso.id})">
                         <ion-icon name="close-circle-outline"></ion-icon>
                     </button>
                 </div>
@@ -98,7 +100,7 @@ const crearEgresoHTML = (egreso) => {
                 <div class="elemento_valor">${formatoMoneda(egreso.valor)}</div>
                 <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor / totalEgresos())}</div>
                 <div class="elemento_eliminar">
-                    <button class="elemento_eliminar_btn" onclick="eliminarEgreso(${egreso.id})">
+                    <button class="elemento_eliminar--btn" onclick="eliminarEgreso(${egreso.id})">
                         <ion-icon name="close-circle-outline"></ion-icon>
                     </button>
                 </div>
